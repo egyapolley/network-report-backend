a68f70be9c67bbea3498db1ee26789fdade3ed21
egyapolley <steven02poli@yahoo.co.uk>
2021-03-13 16:59:20 +0000

    Commit to Push to Prod

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const User = require("../models/users");
 const validate = require("../validators");
 const auth = require("../middlewares/auth");
+const moment = require("moment")
 
 const db = require("../mysql-db/dbcon");
 
@@ -30,63 +31,24 @@ const mme_Counters = {
     VS_UE_TAU_IaMME_all_succ_rate: "VS_UE_TAU_IaMME_all_succ_rate",
     VS_UE_TAU_IaMME_all_succ: "VS_UE_TAU_IaMME_all_succ",
     VS_UE_TAU_IaMME_all_req: "VS_UE_TAU_IaMME_all_req",
-    UECapacityUsage:"MAF Capacity",
-    AveNumOfDefaultBearers:"Avg Number_Default_Bearers",
-    MaxNumOfDefaultBearers:"Max Number_Default_Bearers",
-    AveNumOfDedicatedBearers:"Avg Number_Dedicated_Bearers",
-    MaxNumOfDedicatedBearers:"Max Number_Dedicated_Bearers",
-    AveNbrOfRegisteredUE:"Avg Number_Registered_UEs",
-    MaxNbrOfRegisteredUE:"Max Number_Registered_UEs",
-    AveNbrOfIdleUE:"Max Number_Idle_UEs",
-    MaxNbrOfIdleUE:"Avg Number_Idle_UEs",
-    AveConnectedUE:"Max Number_Connected_UEs",
-    MaxConnectedUE:"Avg Number_Connected_UEs"
+    UECapacityUsage: "MAF Capacity",
+    AveNumOfDefaultBearers: "Avg Number_Default_Bearers",
+    MaxNumOfDefaultBearers: "Max Number_Default_Bearers",
+    AveNumOfDedicatedBearers: "Avg Number_Dedicated_Bearers",
+    MaxNumOfDedicatedBearers: "Max Number_Dedicated_Bearers",
+    AveNbrOfRegisteredUE: "Avg Number_Registered_UEs",
+    MaxNbrOfRegisteredUE: "Max Number_Registered_UEs",
+    AveNbrOfIdleUE: "Max Number_Idle_UEs",
+    MaxNbrOfIdleUE: "Avg Number_Idle_UEs",
+    AveConnectedUE: "Max Number_Connected_UEs",
+    MaxConnectedUE: "Avg Number_Connected_UEs"
 };
-const mme_FormulaReport = ["VS_UE_attach_succ_rate_SFL", "VS_paging_all_rsp_rate_copy_1", "VS_paging_all_rsp",
-        "VS_UE_TAU_IaMME_all_succ_rate", "VS_UE_TAU_IaMME_all_succ", "VS_UE_TAU_IaMME_all_req"];
-
-const pgw_Counters = {
-    NbrSuccessAttachRequests: "Number Of Success Attach Requests",
-    AttAttachRequests: "VS_UE_attach_req",
-    NbrAttachReqAbortBefore: "VS_UE_attach_abort_BeforeAttachCmp",
-    NbrAttachReqAbortAfter: "VS_UE_attach_abort_AfterAttachCmp",
-    NbrFailedAttachRequests_PLMNnotAllowed: "VS_UE_attach_fail_rej_PLMNnotAllowed",
-    NbrFailedAttachRequests_EPSandNonEPSnotAllowed: "VS_UE_attach_fail_rej_EPSandNonEPSnotAllowed",
-    NbrFailedAttachRequests_CannotDeriveUEid: "VS_UE_attach_fail_rej_UEidCannotBeDerived",
-    NbrFailedAttachRequests_NetworkFailure: "VS_UE_attach_fail_rej_NetworkFailure",
-    NbrPageRespInLastSeenTA: "VS_paging_all_rsp_ENBinLastTA",
-    NbrPageRespNotInLastSeenTA: "VS_paging_all_rsp_ENBNotinLastTA",
-    NbrPagingFailures_Timeout: "VS_paging_all_fail_OnMaxRetry",
-    AttPaging_FirstAttempt: "VS_paging_all_req_1stTry",
-    NbrSuccessTAU: "VS_UE_TAU_all_succ",
-    TauInterMmeSucc: "VS_UE_TAU_IrMME_all_succ",
-    AttTAU: "VS_UE_TAU_all_req",
-    TauInterMmeAtt: "VS_UE_TAU_IrMME_all_req",
-    VS_UE_attach_succ_rate_SFL: "VS_UE_attach_succ_rate_SFL",
-    VS_paging_all_rsp_rate_copy_1: "VS_paging_all_rsp_rate_copy_1",
-    VS_paging_all_rsp: "VS_paging_all_rsp",
-    VS_UE_TAU_IaMME_all_succ_rate: "VS_UE_TAU_IaMME_all_succ_rate",
-    VS_UE_TAU_IaMME_all_succ: "VS_UE_TAU_IaMME_all_succ",
-    VS_UE_TAU_IaMME_all_req: "VS_UE_TAU_IaMME_all_req",
-    UECapacityUsage:"MAF Capacity",
-    AveNumOfDefaultBearers:"Avg Number_Default_Bearers",
-    MaxNumOfDefaultBearers:"Max Number_Default_Bearers",
-    AveNumOfDedicatedBearers:"Avg Number_Dedicated_Bearers",
-    MaxNumOfDedicatedBearers:"Max Number_Dedicated_Bearers",
-    AveNbrOfRegisteredUE:"Avg Number_Registered_UEs",
-    MaxNbrOfRegisteredUE:"Max Number_Registered_UEs",
-    AveNbrOfIdleUE:"Max Number_Idle_UEs",
-    MaxNbrOfIdleUE:"Avg Number_Idle_UEs",
-    AveConnectedUE:"Max Number_Connected_UEs",
-    MaxConnectedUE:"Avg Number_Connected_UEs"
-};
-const pgw_FormulaReport = ["VS_UE_attach_succ_rate_SFL", "VS_paging_all_rsp_rate_copy_1", "VS_paging_all_rsp",
+const mme_FormulaCounters = ["VS_UE_attach_succ_rate_SFL", "VS_paging_all_rsp_rate_copy_1", "VS_paging_all_rsp",
     "VS_UE_TAU_IaMME_all_succ_rate", "VS_UE_TAU_IaMME_all_succ", "VS_UE_TAU_IaMME_all_req"];
 
+const pgw_Counters = {};
+const pgw_FormulaReport = [];
 
-
-
-const sessions = {};
 
 router.post("/users", async (req, res) => {
     const {error} = validate.UserRegister(req.body)
@@ -130,12 +92,17 @@ router.get("/data", auth, async (req, res) => {
     if (error) return res.status(400).send(error.message);
 
     const {node, period, reportCounter, startDate, endDate} = req.query
-    await getData(node, period, reportCounter, startDate, endDate, req, res)
+    if (mme_FormulaCounters.includes(reportCounter)) {
+        await get_MME_Data_FormulaCounters(node, period, reportCounter, startDate, endDate, res)
+    } else {
+        await get_MME_Data(node, period, reportCounter, startDate, endDate, res)
+    }
+
 
 })
 
 
-async function getData(node, period, reportCounter, startDate, endDate, req, res) {
+async function get_MME_Data(node, period, reportCounter, startDate, endDate, res) {
 
 
     const query = generateQueryString(node, period, reportCounter);
@@ -143,21 +110,94 @@ async function getData(node, period, reportCounter, startDate, endDate, req, res
         const [rows] = await db.query(query, [startDate, endDate])
         const data = rows.map(item => {
             item.date = formatReportDate(item.date)
-            item.value =parseFloat(item.value)
+            item.value = parseFloat(item.value)
             return item
         })
         const reportName = generateReportName(period, reportCounter)
-        sessions[req.user.uid] = {reportName, data}
         return res.json({reportName, data})
     } catch (ex) {
         console.log(ex);
-        res.status(404).send("Data not found. Please reporting parameters or call sysAdmin")
+        res.status(404).send("No Data not found. Please check reporting parameters or call SysAdmin")
     }
 
 }
 
-function generateReportName(period, reportCounter,node) {
-    let reportName = node ==="pgw"?pgw_Counters[reportCounter]:mme_Counters[reportCounter]
+async function get_MME_Data_FormulaCounters(node, period, reportCounter, startDate, endDate, res) {
+
+    let start_date = startDate;
+    let end_date = endDate;
+    let tableName = "mme2_KPI." + reportCounter;
+
+    let query = "";
+    switch (period) {
+        case "15mins":
+            query = `select   date_format(timeInserted,'%Y%m%d%H%i%s') as date, ifnull(counterValue_1 ,0) as value from ${tableName} where timeInserted between ? and ?`;
+            break;
+
+        case "hourly":
+            start_date = moment(start_date).format("YYYYMMDDHH");
+            end_date = moment(end_date).format("YYYYMMDDHH");
+            tableName = tableName + "_Hourly_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+            break;
+        case "daily":
+            start_date = moment(start_date).format("YYYYMMDD");
+            end_date = moment(end_date).format("YYYYMMDD");
+            tableName = tableName + "_Daily_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+            break;
+
+        case "weekly":
+            start_date = moment(start_date).format("YYYYw");
+            end_date = moment(end_date).format("YYYYw");
+            tableName = tableName + "_Weekly_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+            break;
+
+
+        case "monthly":
+            start_date = moment(start_date).format("YYYYMM");
+            end_date = moment(end_date).format("YYYYMM");
+            tableName = tableName + "_Monthly_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+            break;
+
+        case "yearly":
+            start_date = moment(start_date).format("YYYY");
+            end_date = moment(end_date).format("YYYY");
+            tableName = tableName + "_Yearly_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+            break;
+
+        default:
+            start_date = moment(start_date).format("YYYYMMDDHH");
+            end_date = moment(end_date).format("YYYYMMDDHH");
+            tableName = tableName + "_Hourly_View";
+            query = `select my_date as date, ifnull(value,0) as value from ${tableName} where my_date between ? and ?`;
+
+    }
+
+
+    try {
+
+        const [rows] = await db.query(query, [start_date, end_date])
+        const data = rows.map(item => {
+            item.date = formatReportDate(item.date)
+            item.value = parseFloat(item.value)
+            return item
+        })
+        const reportName = generateReportName(period, reportCounter)
+        return res.json({reportName, data})
+    } catch (ex) {
+        console.log(ex);
+        res.status(404).send("No Data not found. Please check reporting parameters or call SysAdmin")
+    }
+
+
+}
+
+function generateReportName(period, reportCounter, node) {
+    let reportName = node === "pgw" ? pgw_Counters[reportCounter] : mme_Counters[reportCounter]
     return `${reportName}-${period.charAt(0).toUpperCase() + period.substring(1)}`
 
 }
@@ -196,5 +236,6 @@ function generateQueryString(node, period, reportCounter) {
     return query;
 
 }
+
 
 module.exports = router;
